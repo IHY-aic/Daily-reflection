3ada22e74417e2a9fc36e8b713ac14841af0158a
IHY-aic <130772966+IHY-aic@users.noreply.github.com>
2025-08-16 08:17:13 +0900

    Update and rename firebaseConfig.example.js to firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// Copy this file to firebaseConfig.js and replace the values with your Firebase project configuration.
+// Keep firebaseConfig.js private and untracked (see .gitignore).
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDcBAeVyLfzRzn63DVGZcyUnjdw1sqTdhM",
+  authDomain: "reflectionv1.firebaseapp.com",
+  projectId: "reflectionv1",
+  storageBucket: "reflectionv1.firebasestorage.app",
+  messagingSenderId: "932465743002",
+  appId: "1:932465743002:web:7e27fddd9636159a3bfb23",
+  measurementId: "G-NQ80R8XTFD"
+};
